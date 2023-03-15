a952cdef53e5eb1732984cc244ebf80bab4c63a4
Naomi <123805883+NaomiVR@users.noreply.github.com>
2023-03-15 14:05:58 +0000

    Update sn-social-network.rkt

--- sn-social-network.rkt
+++ sn-social-network.rkt
@@ -12,8 +12,8 @@
   
   ;; This simply returns the dict values of the given key using dict-ref
   (define (sn-ff-for graph u1)
-  (dict-ref graph u1)
-  )
+    (dict-ref graph u1)
+    )
 
 
   ;; This first assigns friends1 and 2 as variables containing the list of values for each friends key given, u1 and u2
@@ -23,37 +23,37 @@
   ;; using set-intersect, this creates a set of both sets of friends, and because it's a set it removes
   ;; non-unique values, this is before it converts it to a list and returning it
   (define (sn-cmn-frnds-btwn graph u1 u2)
-  (let* ((friends1 (dict-ref graph u1))
-         (friends2 (dict-ref graph u2))
-         (fofs1 (map (lambda (f) (dict-ref graph f)) friends1))
-         (fofs2 (map (lambda (f) (dict-ref graph f)) friends2))
-         (common-fofs (set->list (set-intersect (apply set-union fofs1)
-                                                (apply set-union fofs2)))))
-    common-fofs)
-  )
+    (let* ((friends1 (dict-ref graph u1))
+           (friends2 (dict-ref graph u2))
+           (fofs1 (map (lambda (f) (dict-ref graph f)) friends1))
+           (fofs2 (map (lambda (f) (dict-ref graph f)) friends2))
+           (common-fofs (set->list (set-intersect (apply set-union fofs1)
+                                                  (apply set-union fofs2)))))
+      common-fofs)
+    )
 
   
   ;; This just returns a count of friends of each user in a given network, using lambda to iterate over the network
   (define (sn-frnd-cnt graph)
-  (map (lambda (entry)
-         (cons (car entry) (length (cdr entry))))
-       graph)
-  )
+    (map (lambda (entry)
+           (cons (car entry) (length (cdr entry))))
+         graph)
+    )
 
   ;; Using sn-frnd-cnt it sorts the returned list by the amount of friends the user has, most being on the left
   ;; before returning the first entry
   (define (sn-frndlst-user graph)
-  (let ((frnd-cnt (sn-frnd-cnt graph)))
-    (car (sort frnd-cnt (lambda (a b) (> (cdr a) (cdr b))))))
-  )
+    (let ((frnd-cnt (sn-frnd-cnt graph)))
+      (car (sort frnd-cnt (lambda (a b) (> (cdr a) (cdr b))))))
+    )
               
           
   ;; This does the same as sn-frndls-user, but the opposite, as it orders them with the least on the left before
   ;; returning that
   (define (sn-unfrndlst-user graph)
-  (let ((frnd-cnt (sn-frnd-cnt graph)))
-    (car (sort frnd-cnt (lambda (a b) (< (cdr a) (cdr b))))))
-  )
+    (let ((frnd-cnt (sn-frnd-cnt graph)))
+      (car (sort frnd-cnt (lambda (a b) (< (cdr a) (cdr b))))))
+    )
 
   ;; this is for free. Do not mdify (ROM)
   (define (sn-cmn-frnds-ff graph u)
